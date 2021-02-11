d6b31758fa757c690ba82cab99460e7add6557fa
даниил банников <goodbish@mail.ru>
2021-02-11 21:43:46 +0500

    feat: code refactor, add adaptive

--- main.js
+++ main.js
@@ -17,10 +17,15 @@ const phis_input = document.querySelector('#phis'),
     agr2_block = document.querySelector('#agreement_2'),
     warning_block = document.querySelector('.agreement__warning'),
     all_forms = document.querySelector('.forms'),
-    final_form = document.querySelector('.final-form')
+    final_form = document.querySelector('.final-form'),
+    go_back_btn = document.querySelector('.accepted__button-left'),
+    // forms
+    choose_form = document.querySelector('.choose__content'),
+    data_form = document.querySelector('.data__content'),
+    agreement_form = document.querySelector('.agreement__form')
 ;
 
-function check(input, block, other_block) {
+let check = (input, block, other_block) => {
     block.classList.toggle("active")
     if (input == phis_input || input == yur_input) {
         other_block.classList.remove("active");
@@ -43,7 +48,7 @@ yur_block.addEventListener('click', function() {check(yur_input, yur_block, phis
 agr1_block.addEventListener('click', function() {check(agr1_input, agr1_block, agr2_block)});
 agr2_block.addEventListener('click', function() {check(agr2_input, agr2_block, agr1_block)});
 
-function nextStep1() {
+let nextStep1 = () => {
     if ((yur_input.checked || phis_input.checked) === true) {
         choose_block.classList.toggle('active');
         data_block.classList.toggle('active');
@@ -51,10 +56,12 @@ function nextStep1() {
         choose_number.style.backgroundColor = '#1BC400';
         choose_title.style.color = '#3B4256';
         choose_number.innerHTML = '<img src="./assets/done_sign.svg" alt="done">'
+
+
     }
 }
 
-function nextStep2() {
+let nextStep2 = () => {
     data_block.classList.toggle('active');
     agreement_block.classList.toggle('active');
 
@@ -63,9 +70,28 @@ function nextStep2() {
     data_number.innerHTML = '<img src="./assets/done_sign.svg" alt="done">'
 }
 
-function nextStep3() {
+let nextStep3 = () => {
     if ((agr1_input.checked && agr2_input.checked) == true) {
         all_forms.style.display = 'none';
         final_form.style.display = 'flex';
     }
 }
+
+let goBack = () => {
+    document.location.reload();
+}
+
+go_back_btn.addEventListener('click', goBack)
+
+choose_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    nextStep1();
+});
+data_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    nextStep2();
+});
+agreement_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    nextStep3();
+});
